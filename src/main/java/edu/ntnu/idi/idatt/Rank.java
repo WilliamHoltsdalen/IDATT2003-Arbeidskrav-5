4bb927952b594ce135665263382601f102199dc5
WilliamHoltsdalen <williamlh@hotmail.no>
2025-03-01 12:55:00 +0100

    feat(game): Add enums for card suit and rank

---
+++ src/main/java/edu/ntnu/idi/idatt/Rank.java
@@ -0,0 +1,8 @@
+package edu.ntnu.idi.idatt;
+
+/**
+ * Represents a rank of playing cards. The ranks are ordered numerically.
+ */
+public enum Rank {
+  ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+}
---
+++ src/main/java/edu/ntnu/idi/idatt/Suit.java
@@ -0,0 +1,8 @@
+package edu.ntnu.idi.idatt;
+
+/**
+ * Represents a suit of playing cards. The suits are ordered alphabetically.
+ */
+public enum Suit {
+  CLUBS, DIAMONDS, HEARTS, SPADES
+}
