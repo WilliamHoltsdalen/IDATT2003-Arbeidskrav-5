5467d855c0c023512462c7e2021939b368a42eb0
WilliamHoltsdalen <williamlh@hotmail.no>
2025-03-01 22:16:07 +0100

    feat(ui): Add view for card area with demo images

---
+++ src/main/java/edu/ntnu/idi/idatt/view/CardAreaView.java
@@ -0,0 +1,40 @@
+package edu.ntnu.idi.idatt.view;
+
+import javafx.geometry.Pos;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardAreaView extends StackPane{
+
+  public CardAreaView() {
+    setStyle("-fx-background-color: #8B4513; -fx-padding: 20; -fx-background-radius: 15;");
+
+    StackPane greenFeltPane = new StackPane();
+    greenFeltPane.setStyle("-fx-background-color: #006400; -fx-padding: 20; -fx-background-radius: 10;");
+
+    // Card display area
+    HBox cardBox = new HBox(10);
+    cardBox.setAlignment(Pos.CENTER);
+    greenFeltPane.getChildren().add(cardBox);
+
+    List<String> cardNames = new ArrayList<>();
+    cardNames.add("images/queen_of_spades.png");
+    cardNames.add("images/queen_of_hearts.png");
+    cardNames.add("images/queen_of_diamonds.png");
+    cardNames.add("images/queen_of_clubs.png");
+    cardNames.add("images/jack_of_hearts.png");
+    for (String cardName : cardNames) {
+      ImageView cardImage = new ImageView(cardName);
+      cardImage.setFitWidth(100);
+      cardImage.setFitHeight(100);
+      cardImage.setPreserveRatio(true);
+      cardBox.getChildren().add(cardImage);
+    }
+
+    getChildren().add(greenFeltPane);
+  }
+}
